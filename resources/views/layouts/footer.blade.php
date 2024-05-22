3dd0f542baee7adab9265690c3a65b1ed6d215da
Driann's <114148680+Driannm@users.noreply.github.com>
2024-05-22 13:10:46 +0700

    Update: Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
   <div class="container my-auto">
     <div class="copyright text-center my-auto">
-      <span>Copyright Poltek Harber | Danang Setya Adhitama</span>
+      <span>Copyright Poltek Harber | Yuda Andrian</span>
     </div>
   </div>
 </footer>
